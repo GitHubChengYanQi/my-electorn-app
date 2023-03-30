9e1edd67c9ade4ec8701dc4b456162ffc9549ca4
cheng <2683941980@qq.com>
2023-03-30 16:55:33 +0800

    完成填写数据

--- main.js
+++ main.js
@@ -1,26 +1,61 @@
-const {app, BrowserWindow, ipcMain, session} = require('electron')
+const {app, BrowserWindow, ipcMain, session, Menu} = require('electron')
 const path = require('path')
 
 let filePath = path.resolve(path.join(__dirname, './lib/preload.js'))
 
 let newMianWindow;
 
+let template = [
+    {
+        label: '刷新(Ctrl+R)',
+        accelerator: 'CmdOrCtrl+R',
+        click: function (item, focusedWindow) {
+            if (focusedWindow) {
+                // on reload, start fresh and close any old
+                // open secondary windows
+                if (focusedWindow.id === 1) {
+                    BrowserWindow.getAllWindows().forEach(function (win) {
+                        if (win.id > 1) {
+                            win.close()
+                        }
+                    })
+                }
+                focusedWindow.reload()
+            }
+        }
+    }
+]
+
+let actionWindow = null
+
+let loadingWindow = null
+
 const createMainWindow = (url) => {
 
+    loadingWindow = createLoadingWindow()
+
     const win = new BrowserWindow({
+        icon: path.resolve(path.join(__dirname, './public/imgs/weisheng.png')),
         width: 1920,
         height: 1080,
+        show: false,
         webPreferences: {
+            nodeIntegrationInSubFrames: true,
             nodeIntegration: true,
             webviewTag: true,
             preload: filePath,
             contextIsolation: false
         }
     })
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
     win.loadURL(url || 'https://zb.lnwsjktj.com:8080/webPage/esmain/login.do')
-    win.once('ready-to-show', () => {
+
+    win.webContents.on('dom-ready', () => {
         win.show()
+        if (loadingWindow) {
+            loadingWindow.close()
+            loadingWindow = null
+        }
     })
 
 
@@ -66,7 +101,27 @@ const createMainWindow = (url) => {
             newMianWindow.close()
         }
         newMianWindow = createMainWindow(details.url)
+        newMianWindow.on('show', () => {
+            if (!actionWindow) {
+                actionWindow = createActionView()
+                ipcMain.handle('append', (event, args) => {
+                    console.log('append==>', args)
+                    actionWindow.webContents.send('queryList', args)
+                })
+                actionWindow.on('close', () => {
+                    if (actionWindow) {
+                        ipcMain.removeHandler('append')
+                        actionWindow = null
+                    }
+                })
+            }
+        })
         newMianWindow.on('close', () => {
+            if (actionWindow) {
+                actionWindow.close()
+                ipcMain.removeHandler('append')
+                actionWindow = null
+            }
             if (newMianWindow) {
                 newMianWindow = null
             }
@@ -81,9 +136,36 @@ const createMainWindow = (url) => {
 }
 
 const createActionView = () => {
+
     const win = new BrowserWindow({
-        width: 800,
+        icon: path.resolve(path.join(__dirname, './public/imgs/weisheng.png')),
+        width: 500,
+        maxWidth: 500,
         height: 800,
+        maximizable: false,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true,
+            preload: filePath,
+            contextIsolation: true
+        }
+    })
+
+    win.setAlwaysOnTop(true)
+    win.loadFile(path.resolve(path.join(__dirname, './action/index.html')))
+    // win.loadURL('http://localhost:8000')
+
+    return win
+}
+
+const createLoadingWindow = () => {
+    const win = new BrowserWindow({
+        width: 500,
+        maxWidth: 500,
+        height: 500,
+        frame: false,
+        maximizable: false,
+        transparent: true,
         webPreferences: {
             nodeIntegration: true,
             webviewTag: true,
@@ -93,26 +175,22 @@ const createActionView = () => {
     })
     win.menuBarVisible = false
     win.setAlwaysOnTop(true)
-    // win.setPosition(1620, 0)
-    // win.webContents.openDevTools()
-    // win.loadFile(path.resolve(path.join(__dirname, './/action/index.html')))
-    win.loadURL('http://localhost:8000')
-
+    win.loadFile(path.resolve(path.join(__dirname, './page/index.html')))
     return win
 }
 
 
 app.whenReady().then(() => {
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
 
     const mainWindow = createMainWindow()
+
     ipcMain.handle('LoadData', (event, args) => {
         console.log('getData==>', args)
         if (newMianWindow) {
             newMianWindow.webContents.send('post', args)
         }
-    })
-    mainWindow.on('show', () => {
-        const actionWindow = createActionView()
     })
 
 })
